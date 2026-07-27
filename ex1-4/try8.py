bf92fec6262c9c9193bf9e07aa6317b588909367
MariglenCullhaj <mariglen.cullhaj@gmail.com>
2026-07-27 12:21:59 +0200

    exam sucesgul first try

---
+++ ex1-4/try8.py
@@ -0,0 +1,17 @@
+# ex1-4 - string_sculptor
+# attempt: try8.py
+# (signature pre-filled from the .en; write your solution below)
+
+def string_sculptor(text: str) -> str:
+    result = ""
+    i = 0
+    for char in text:
+        if char.isalpha():
+            if i % 2 == 0:
+                result += char.lower()
+            else:
+                result += char.upper()
+            i += 1
+        else:
+            result += char
+    return result
---
+++ ex4/try10.py
@@ -0,0 +1,21 @@
+# ex4 - cryptic_sort
+# attempt: try10.py
+# (signature pre-filled from the .en; write your solution below)
+def count_vouels(word):
+    count = 0
+    vouels = "aeiouAEIOU"
+    for char in word:
+        if char in vouels:
+            count += 1
+    return count
+
+def rank_values(word):
+    word_len = len(word)
+    word_low = word.lower()
+    word_vow = count_vouels(word)
+    
+    return (word_len, word_low, word_vow)
+
+
+def cryptic_sort(strings: list[str]) -> list:
+    return sorted(strings, key=rank_values)
